16070afa37d51a140d974320def1e4d62aa544c3
Tino Breddin <t@tolbrino.com>
2020-07-07 10:27:33 +0200

    Set color

--- pure.zsh
+++ pure.zsh
@@ -787,7 +787,7 @@ prompt_pure_setup() {
 		prompt:continuation  242
 		user                 242
 		user:root            default
-		virtualenv           242
+		virtualenv           blue
 	)
 	prompt_pure_colors=("${(@kv)prompt_pure_colors_default}")
 
